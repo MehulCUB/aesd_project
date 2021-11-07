65686f2bcac4d3784677b63814487a199a3c60be
MehulCUB <mehulkumar.patel@colorado.edu>
2021-11-07 12:29:35 -0700

    correct indetation

--- build.sh
+++ build.sh
@@ -43,10 +43,10 @@ bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
 layer_info=$?
 
 if [ $layer_info -ne 0 ];then
-        echo "Adding meta-raspberrypi layer"
-        bitbake-layers add-layer ../meta-raspberrypi
+	echo "Adding meta-raspberrypi layer"
+	bitbake-layers add-layer ../meta-raspberrypi
 else
-        echo "meta-raspberrypi layer already exists"
+	echo "meta-raspberrypi layer already exists"
 fi
 
 
